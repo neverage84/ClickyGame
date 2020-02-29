64af766d140e828e2083c57f184f1664ff33f293
Noelle Everage <neverage84@yahoo.com>
2020-02-28 19:20:49 -0800

    Created game component which will be the hub for all components

---
+++ clicky_game/src/components/Game/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavBar from "../NavBar"
+
+class Game extends Component{
+
+    render(){
+        return (
+            <NavBar />
+        )
+    }
+
+}
+
+export default Game;
