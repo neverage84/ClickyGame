97d4bc51f2b4f8fb389c342b0674b7291b34c52e
Noelle Everage <neverage84@yahoo.com>
2020-02-29 15:49:04 -0800

    Fixed some minor spacing/grammar aspects'

--- clicky_game/src/components/Sliders/index.js
+++ clicky_game/src/components/Sliders/index.js
@@ -12,14 +12,14 @@ function Slider(props){
                 <li>
                     <img alt="Backdrop" src={hogwarts}/>
                     <div className="caption center-align">
-                        <h1>CLICKY GAME</h1>
+                        <h1>CLICKY GAME!</h1>
                         <h2>Click on an image to earn points, but don't click on any more than once!</h2>
                     </div>
                 </li>
                 <li>
                     <img alt="Backdrop" src={hallway}/>
                     <div className="caption center-align">
-                        <h3>Good luck Muggle!</h3>
+                        <h1>Good luck, Muggle!</h1>
                     </div>
                 </li>
             </ul>
