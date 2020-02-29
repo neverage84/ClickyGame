b425d103f277a7a916f4da548b221fe360da8cdc
Noelle Everage <neverage84@yahoo.com>
2020-02-29 15:11:30 -0800

    Added images for slider and HP logo, importing to react components. Adjuste Nav dimensions. Added Materialize slider jquery initialize script

--- clicky_game/src/components/NavBar/index.js
+++ clicky_game/src/components/NavBar/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css"
+import HarryPotter from "../../images/HPLogo.png"
 
 function NavBar(props){
     return (
-    <nav className="black">
+    <nav className="brown darken-4 NavDim">
         <div className="nav-wrapper">
-        <a href="#" className="brand-logo center">Harry Potter</a>
+        <a href="#" className="brand-logo center"><img className="HPLogo" src={HarryPotter} alt="HarryPotter"/></a>
     </div>
   </nav>
   
--- clicky_game/src/components/Sliders/index.js
+++ clicky_game/src/components/Sliders/index.js
@@ -1,14 +1,25 @@
 import React from "react";
+import hogwarts from "../../images/hogwarts.jpg";
+import hallway from "../../images/hallway.jpg";
+
 
 function Slider(props){
     return (
+      
     <div>
         <div className="slider">
             <ul className="slides">
                 <li>
-                    <img alt="Backdrop"src="https://images3.alphacoders.com/205/205895.jpg"></img>
+                    <img alt="Backdrop" src={hogwarts}/>
                     <div className="caption center-align">
-                        <h3>CLICKY GAME</h3>
+                        <h1>CLICKY GAME</h1>
+                        <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+                    </div>
+                </li>
+                <li>
+                    <img alt="Backdrop" src={hallway}/>
+                    <div className="caption center-align">
+                        <h3>Good luck Muggle!</h3>
                     </div>
                 </li>
             </ul>
