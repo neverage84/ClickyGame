a16acf00fdbc61381acbcf0bf0e4486f85f30522
Noelle Everage <neverage84@yahoo.com>
2020-02-28 20:00:54 -0800

    Added react package and auto initialized jquery materialize items

--- clicky_game/src/components/Game/index.js
+++ clicky_game/src/components/Game/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import NavBar from "../NavBar"
+import M from "materialize-css";
 
 class Game extends Component{
+
+    
+    //Materialize
+    componentDidMount() {
+        // Auto initialize all the things!
+        M.AutoInit();
+    }
 
     render(){
         return (
--- clicky_game/src/components/NavBar/index.js
+++ clicky_game/src/components/NavBar/index.js
@@ -5,7 +5,7 @@ function NavBar(props){
     return (
     <nav>
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">Logo</a>
+      <a className="brand-logo">Logo</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="sass.html">{props.children}</a></li>
