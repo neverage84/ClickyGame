7e52d8ad0a68f3b323553978ccb2ae33a62286fd
Noelle Everage <neverage84@yahoo.com>
2020-02-29 16:03:13 -0800

    Added function to generate Charater Card

---
+++ clicky_game/src/components/Characters/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css"
+
+function CharacterCard(props){
+return(
+<div className="container">
+    <div class="row">
+        <div className="col s3 m7">
+            <div className="card">
+                <div className="card-image">
+                    <img alt={props.name} src={props.image} />
+                    <span class="card-title">{props.title}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+);
+}
