ac1744b44d478b4c6ea7662b25805bafba812d88
Noelle Everage <neverage84@yahoo.com>
2020-03-03 15:03:24 -0800

    Added background song component. Revised Click events to array.map vs for loop. added separate correct guess vs incorrect guess handlers

--- clicky_game/src/components/Game/index.js
+++ clicky_game/src/components/Game/index.js
@@ -12,35 +12,84 @@ class Game extends Component{
     state = {
         characters,
         CurrentScore: 0,
-        TopScore: 0
+        TopScore: 0,
+        Guess: false
     }
+    constructor(props) {
+        super(props);
+        this.url = "Song.mp3";
+        this.audio = new Audio(this.url);
+      }
+     play = () => {
+    console.log(this.audio);
+    this.audio.play();
+     }
+
+
     
-  
-    
-    //Materialize
     componentDidMount() {
+        //Materialize
         // Auto initialize all the things!
         M.AutoInit();
-        this.setState({ characters : this.shuffleCHAR(this.state.characters) });
+        this.setState({ characters : this.shuffleData(this.state.characters) });
     }
 
-   CardClick = id => {
-    console.log("CLICKED: " + id);
-    // audio.play();
-    this.setState({characters: this.shuffleCHAR(this.state.characters)});
-   }
-
-    shuffleCHAR = characters => {
-        let i = characters.length -1;
+    handleCorrectGuess = newData => {
+        const { TopScore, CurrentScore } = this.state;
+        const newScore = CurrentScore + 1;
+        const newTopScore = Math.max(newScore, TopScore);
+       this.play();
+    
+        this.setState({
+          characters: this.shuffleData(newData),
+          CurrentScore: newScore,
+          TopScore: newTopScore
+        });
+      };
+    
+      handleIncorrectGuess = data => {
+        this.setState({
+          characters: this.resetData(data),
+          CurrentScore: 0
+        });
+      };
+    
+      resetData = data => {
+        const resetData = data.map(item => ({ ...item, clicked: false }));
+        return this.shuffleData(resetData);
+      };
+    
+      shuffleData = data => {
+        let i = data.length - 1;
         while (i > 0) {
-            let j = Math.floor(Math.random()*( i + 1));
-            let temp = characters[i];
-            characters[i] = characters[j];
-            characters[j] = temp;
-            i--;
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = data[i];
+          data[i] = data[j];
+          data[j] = temp;
+          i--;
         }
-    return characters;
-    }
+        return data;
+      };
+    
+      CardClick = id => {
+          
+        let guessedCorrectly = false;
+        const newData = this.state.characters.map(item => {
+          const newItem = { ...item };
+          if (newItem.id === id) {
+            if (!newItem.clicked) {
+              newItem.clicked = true;
+              guessedCorrectly = true;
+              
+            }
+          }
+          return newItem;
+        });
+        guessedCorrectly
+          ? this.handleCorrectGuess(newData)
+          : this.handleIncorrectGuess(newData);
+      };
+
 
     render(){
         return (
@@ -49,7 +98,7 @@ class Game extends Component{
                 CurrentScore={this.state.CurrentScore}
                 TopScore={this.state.TopScore} />
                 <Slider />
-                <Container> 
+                <Container onClick={this.play}> 
                 {this.state.characters.map(characters =>( 
                 <CharacterCard 
                     key={characters.id}
